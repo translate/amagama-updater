ba7e9803e8fa818ea392d6e4815b817f5f871e48
Leandro Regueiro <leandro.regueiro@gmail.com>
2014-06-27 18:13:46 +0200

    Uncomment necessary code

--- update-debian.sh
+++ update-debian.sh
@@ -7,7 +7,7 @@ debian_download="$data_root/debian-download"
 
 mkdir -p $debian_root
 
-#wget --mirror --no-parent --reject=*-stamp*.po -P $debian_download  http://i18n.debian.net/compendia/po/
+wget --mirror --no-parent --reject=*-stamp*.po -P $debian_download  http://i18n.debian.net/compendia/po/
 
 find $debian_download/i18n.debian.net/compendia/po -name compendium-*.po | while read i; do
 	msggrep -v --msgid -e '#-#-#-' $i > $debian_root/`basename $i`
