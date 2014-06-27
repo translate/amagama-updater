6858e6d91af84f6da07d2a0bfb1536899c9f1ce1
Leandro Regueiro <leandro.regueiro@gmail.com>
2014-06-27 18:16:30 +0200

    Get rid of now unnecessary function

--- update-fedora.sh
+++ update-fedora.sh
@@ -7,15 +7,11 @@ fedora_root="$data_root/fedora"
 mkdir -p $fedora_root
 cd $fedora_root
 
-do_stuff () {
-	for m in $@; do
+for offset in `seq 0 50 400`; do
+	modules=`wget --no-proxy -o /dev/null -O- 'http://git.fedorahosted.org/cgit/?s=idle&ofs='$offset | grep 'level-repo[^~]*$' | sed "s/^.*href='\([^']*\)'.*$/\1/"`
+	for m in $modules; do
 		project_root=`echo $m | sed 's/\/cgit\/\(.*\)\/tree\/$/\1/'`
 		GIT_URL=git://git.fedorahosted.org/$project_root
 		update_git
 	done
-}
-
-for offset in `seq 0 50 400`; do
-	modules=`wget --no-proxy -o /dev/null -O- 'http://git.fedorahosted.org/cgit/?s=idle&ofs='$offset | grep 'level-repo[^~]*$' | sed "s/^.*href='\([^']*\)'.*$/\1/"`
-	do_stuff $modules
 done
