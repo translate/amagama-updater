b35df540b0643ba8abf2b1a0199655e7d85a87de
Leandro Regueiro <leandro.regueiro@gmail.com>
2014-06-27 16:07:16 +0200

    Use another variable

--- update-debian.sh
+++ update-debian.sh
@@ -3,15 +3,16 @@
 . `dirname $0`/common.inc.sh
 
 debian_root="$data_root/debian"
+debian_download="$data_root/debian-download"
 
 mkdir -p $debian_root
-mkdir -p $debian_root-download
+mkdir -p $debian_download
 
 pushd .
-#cd $debian_root-download
+#cd $debian_download
 #wget --mirror --no-parent --reject=*-stamp*.po http://i18n.debian.net/compendia/po/
 popd
 
-find $debian_root-download/i18n.debian.net/compendia/po -name compendium-*.po| while read i; do
+find $debian_download/i18n.debian.net/compendia/po -name compendium-*.po | while read i; do
 	msggrep -v --msgid -e '#-#-#-' $i > $debian_root/`basename $i`
 done
