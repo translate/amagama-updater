cdfc6c90d810b5837b7d03a94fd4b2853e223da5
Leandro Regueiro <leandro.regueiro@gmail.com>
2014-06-27 18:14:14 +0200

    Change code to somewhat easier to read

--- update-debian.sh
+++ update-debian.sh
@@ -9,6 +9,7 @@ mkdir -p $debian_root
 
 wget --mirror --no-parent --reject=*-stamp*.po -P $debian_download  http://i18n.debian.net/compendia/po/
 
-find $debian_download/i18n.debian.net/compendia/po -name compendium-*.po | while read i; do
+compendia=`find $debian_download/i18n.debian.net/compendia/po -name compendium-*.po`
+for i in $compendia; do
 	msggrep -v --msgid -e '#-#-#-' $i > $debian_root/`basename $i`
 done
