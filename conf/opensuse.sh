7c36884e526740da5b006c3095089fc9eff7d98e
Dwayne Bailey <dwayne@translate.org.za>
2014-11-14 23:55:26 +0000

    project: add openSUSE

---
+++ conf/opensuse.sh
@@ -0,0 +1,3 @@
+project=opensuse
+repo=svn+http://svn.opensuse.org/svn/opensuse-i18n/trunk/
+layout=gnu
--- update-opensuse.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-. `dirname $0`/common.inc.sh
-
-project_root="$data_root/opensuse-i18n"
-SVN_URL=http://svn.opensuse.org/svn/opensuse-i18n/trunk/
-update_svn
