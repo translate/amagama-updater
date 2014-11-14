3584d15e2dec88c131c16b25a0d013970b7e97b4
Dwayne Bailey <dwayne@translate.org.za>
2014-11-14 23:54:51 +0000

    project: add KDE

---
+++ conf/kde.sh
@@ -0,0 +1,3 @@
+project=kde
+repo=svn+svn://anonsvn.kde.org/home/kde/trunk/l10n-kde4/
+layout=nongnu
--- update-kde.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-. `dirname $0`/common.inc.sh
-
-project_root="$data_root/l10n-kde4"
-SVN_URL=svn://anonsvn.kde.org/home/kde/trunk/l10n-kde4/
-update_svn
