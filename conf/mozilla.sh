04bf06c42e6b6469b61a171755bb2e6551f7232d
Dwayne Bailey <dwayne@translate.org.za>
2014-11-18 17:29:26 +0000

    project: add Mozilla
    Gecko, Firefox OS and web parts

---
+++ conf/mozilla.sh
@@ -0,0 +1,13 @@
+project=mozilla
+
+subproject[0]=firefox
+repo[0]=git+git://github.com/translate/mozilla-l10n.git
+layout[0]=nongnu
+
+subproject[1]=web_parts
+repo[1]=git+git://github.com/translate/mozilla-lang.git
+layout[1]=nongnu
+
+subproject[2]=fxos
+repo[2]=git+git://github.com/translate/mozilla-gaia.git
+layout[2]=nongnu
