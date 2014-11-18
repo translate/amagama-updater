4acc0e81ba71c74984781ba4101163137feae881
Dwayne Bailey <dwayne@translate.org.za>
2014-11-18 16:23:56 +0000

    projects: add Pidgin

---
+++ conf/pidgin.sh
@@ -0,0 +1,4 @@
+project=pidgin
+repo=hg+http://hg.pidgin.im/pidgin/main
+layout=gnu
+translation_dirs=po
--- update-pidgin.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-. `dirname $0`/common.inc.sh
-
-project_root="$data_root/pidgin"
-HG_URL=http://hg.pidgin.im/pidgin/main
-update_hg
