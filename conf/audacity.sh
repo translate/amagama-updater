f41a9edbf5d281f049e0b8367d8a74653cb370ea
Dwayne Bailey <dwayne@translate.org.za>
2014-11-18 12:52:15 +0000

    project: add Audacity

---
+++ conf/audacity.sh
@@ -0,0 +1,9 @@
+project=audacity
+
+subproject[0]=code
+repo[0]=svn+http://audacity.googlecode.com/svn/audacity-src/trunk/locale
+layout[0]=gnu
+
+subproject[1]=website
+repo[1]=svn+http://audacity.googlecode.com/svn/website/trunk/htdocs/locale
+layout[1]=gnu
