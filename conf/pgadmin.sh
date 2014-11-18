72b08d5170ed4c702ae93e5351998ff68346f1e3
Dwayne Bailey <dwayne@translate.org.za>
2014-11-18 17:20:50 +0000

    project: add pgadmin

---
+++ conf/pgadmin.sh
@@ -0,0 +1,11 @@
+project=pgadmin
+
+subproject[0]=pgadmin
+repo[0]=git+git://git.postgresql.org/git/pgadmin3.git
+layout[0]=nongnu
+translated_dirs[0]=i18n
+
+subproject[1]=www
+repo[1]=git+git://git.postgresql.org/git/pgadmin-www.git
+layout[1]=nongnu
+translated_dirs[1]=locale
