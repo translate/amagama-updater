192181b63596bc8190d53855cb01e8dac7c3b415
Dwayne Bailey <dwayne@translate.org.za>
2014-11-19 19:37:27 +0000

    projects: add some test language mappings

--- conf/abiword.sh
+++ conf/abiword.sh
@@ -1,3 +1,4 @@
 project=abiword
 repo=svn+http://svn.abisource.com/abiword/trunk/po 
 layout=gnu
+language_mapping=af-ZA:af
--- conf/pgadmin.sh
+++ conf/pgadmin.sh
@@ -1,5 +1,7 @@
 project=pgadmin
 
+language_mapping="af_ZA:af fr_FR:fr it_IT:it pl_PL:pl ru_RU:ru"
+
 subproject[0]=pgadmin
 repo[0]=git+git://git.postgresql.org/git/pgadmin3.git
 layout[0]=nongnu
