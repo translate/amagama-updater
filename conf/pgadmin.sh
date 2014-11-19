48894a8da9ee7e3887327323c9f3dd964d02036b
Dwayne Bailey <dwayne@translate.org.za>
2014-11-19 19:37:55 +0000

    project: fix pgadmin translation_dirs config

--- conf/pgadmin.sh
+++ conf/pgadmin.sh
@@ -5,9 +5,9 @@ language_mapping="af_ZA:af fr_FR:fr it_IT:it pl_PL:pl ru_RU:ru"
 subproject[0]=pgadmin
 repo[0]=git+git://git.postgresql.org/git/pgadmin3.git
 layout[0]=nongnu
-translated_dirs[0]=i18n
+translation_dirs[0]=i18n
 
 subproject[1]=www
 repo[1]=git+git://git.postgresql.org/git/pgadmin-www.git
 layout[1]=nongnu
-translated_dirs[1]=locale
+translation_dirs[1]=locale
