19d213a3effa0e9308ad333805c11fd76d8a3be8
Dwayne Bailey <dwayne@translate.org.za>
2014-11-19 19:36:25 +0000

    import: add initial iteration of import
    Doesn't import at all just displays the amagama import line.

--- common.inc.sh
+++ common.inc.sh
@@ -95,3 +95,96 @@ update() {
 		update_$vcs_type
 	fi
 }
+
+language_code() {
+	# Return the correct langauge code for this language
+	code=$1
+	for l in $language_mapping
+	do
+		from=$(echo $l | cut -d":" -f1)
+		to=$(echo $l | cut -d":" -f2)
+		if [[ $code == $from ]]; then
+			echo $to
+			return
+		fi
+	done
+	echo $code
+}
+
+maxjobs=8
+amagama_loader() {
+	while [ $# -gt 0 ]; do
+		count=(`jobs -p`)
+		if [ ${#count[@]} -lt $maxjobs ]; then
+			file=$1
+			lang=$(language_code $(basename $file .po))
+			echo "~/amagama/amagama/bin/amagama-manage build_tmdb -s en -t $lang -i $file &"
+			shift
+		else
+			sleep 1
+		fi
+	done
+	wait
+}
+
+import_gnu() {
+	echo "Gnu $1"
+	if [[ ! -d $1 ]]; then
+		echo "Error: missing directory '$1'"
+		return
+	fi
+	amagama_loader $(find $1 -name "*.po" -print | sort -u)
+}
+
+import_nongnu() {
+	echo "nonGnu $1"
+	if [[ ! -d $1 ]]; then
+		echo "Error: missing directory '$1'"
+		return
+	fi
+	amagama_loader $(find $1 -type d -depth 1 -print | sort -u)
+}
+
+import_project() {
+	layout=$1
+	shift
+	translation_dirs=$*
+	if [[ $translation_dirs ]]; then
+		for translation_dir in $translation_dirs
+		do
+			if [[ $layout == "gnu" ]]; then
+				import_gnu $project_root/$translation_dir
+			else
+				import_nongnu $project_root/$translation_dir
+			fi
+		done
+	else
+		if [[ $layout == "gnu" ]]; then
+			import_gnu $project_root
+		else
+			import_nongnu $project_root
+		fi
+	fi
+}
+
+import() {
+	echo $project
+	if [[ ${#subproject[@]} -gt 0 ]]; then
+		for index in $(seq 0 $((${#subproject[@]} - 1)))
+		do
+			echo "- ${subproject[index]}"
+			project_root=$data_root/$project/${subproject[index]}
+			import_project ${layout[index]} ${translation_dirs[index]}
+		done
+	elif [[ $iterator ]]; then
+		for iter in $iterator
+		do
+			echo "- $iter"
+			project_root=$data_root/$project/$iter
+			import_project $layput $translation_dirs
+		done
+	else
+		project_root=$data_root/$project
+		import_project $layout $translation_dirs
+	fi
+}
