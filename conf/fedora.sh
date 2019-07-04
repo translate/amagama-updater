6ef732b87213c5d154ba1e22238daacb171183a3
Friedel Wolff <friedel@translate.org.za>
2019-07-04 14:05:11 +0200

    Rewrite configuration for Fedora
    Fedorahosted.org is gone, and it isn't obvious that cloning everything
    from pagure.io is worthwhile. Lots of things seem missing, so this should
    get more attention later. See https://pagure.io/g11n/issue/59

--- conf/fedora.sh
+++ conf/fedora.sh
@@ -1,12 +1,13 @@
 project=fedora
 
-iterator=$(
-for offset in `seq 0 50 400`; do
-	modules=`wget --no-proxy -o /dev/null -O- 'http://git.fedorahosted.org/cgit/?s=idle&ofs='$offset | grep 'level-repo[^~]*$' | sed "s/^.*href='\([^']*\)'.*$/\1/"`
-	for m in $modules; do
-		echo $m | sed 's/\/cgit\/\(.*\)\/tree\/$/\1/'
-	done
-done
-)
+# Lots missing here. See https://pagure.io/g11n/issue/59
 
-repo="git+git://git.fedorahosted.org/<iterator>"
+subproject[0]=fedora-websites
+repo[0]=git+https://pagure.io/fedora-websites.git
+layout[0]=gnu
+translation_dirs[0]="fedoraproject.org/po spins.fedoraproject.org/po start.fedoraproject.org/po budget.fedoraproject.org/po getfedora.org/po fudcon.fedoraproject.org/po flocktofedora.org/po alt.fedoraproject.org/po"
+
+subproject[1]=fedora-comps
+repo[1]=git+https://pagure.io/fedora-comps.git
+layaout[1]=gnu
+translation_dirs[1]=po
